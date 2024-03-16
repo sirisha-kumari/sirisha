90788e5304ba12239369203c5f8bb606cb4453a1
sirisha-kumari <163641232+sirisha-kumari@users.noreply.github.com>
2024-03-16 15:38:42 +0530

    Add files via upload

---
+++ func1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:58:53 2024
+
+@author: SIRISHA KUMARI
+"""
+#default arguments
+def values(s=4,t=5):
+    print(s,t)
+values()
+values(s=3,t=2)
